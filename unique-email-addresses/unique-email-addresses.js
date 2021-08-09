32701945ce58929d863bb6e01ca6a951c0b8a580
Michael Palmer <33425993+gitInLoserWereGoingShopping@users.noreply.github.com>
2021-08-09 18:20:33 -0500

    80 ms, faster than 95.22% and 44 MB, less than 42.31% of JS submissions
    cleaned up code and improved run time

--- unique-email-addresses/unique-email-addresses.js
+++ unique-email-addresses/unique-email-addresses.js
@@ -11,42 +11,20 @@
  
 */
 const numUniqueEmails = (emails) => {
-  if (emails.length >= 1 && emails.length <= 100) {
-    let counter = 0;
-    let validEmails = {};
-    emails.forEach(email => {
-      if (
-        (email.length >= 1 && email.length <= 100) &&
-        (email === email.toLowerCase()) &&
-        (email.includes('.') && email.includes('@')) &&
-        (email.indexOf('@') === email.lastIndexOf('@')) &&
-        (email[0] !== '+')
-      ) {
-        //split left (local) and right (domain) of '@' in email
-        let atIndex = email.indexOf('@');
-        //set local name and remove '.' out of it
-        let local = email.slice(0, atIndex).split('.').join('');
-        let domain = email.slice(atIndex);
-        if (local.length && domain.length) {
-          if (local.includes('+')) {
-            local = local.slice(0, local.indexOf('+'))
-            let modifiedEmail = local + '@' + domain;
-            if (!validEmails[modifiedEmail]) {
-              validEmails[modifiedEmail] = true;
-              counter++;
-            }
-          } else {
-            let modifiedEmail = local + '@' + domain;
-            if (!validEmails[modifiedEmail]) {
-              validEmails[modifiedEmail] = true;
-              counter++;
-            }
-          }
-        }
-      }
-    })
-    return counter;
-  } else {
-    return 0;
-  }
+  let counter = 0;
+  let validEmails = {};
+  emails.forEach(email => {
+    let atIndex = email.indexOf('@');
+    let local = email.slice(0, atIndex).split('.').join('');
+    let domain = email.slice(atIndex);
+    if (local.includes('+')) {
+      local = local.slice(0, local.indexOf('+'))
+    }
+    let modifiedEmail = local + '@' + domain;
+    if (!validEmails[modifiedEmail]) {
+      validEmails[modifiedEmail] = true;
+      counter++;
+    }
+  })
+  return counter;
 };
