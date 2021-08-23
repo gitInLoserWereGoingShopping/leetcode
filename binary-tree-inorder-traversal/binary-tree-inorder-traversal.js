ad80300d5bbf7f44fd23170b7a8314b9535f7536
Michael Palmer <33425993+gitInLoserWereGoingShopping@users.noreply.github.com>
2021-08-23 10:49:55 -0500

    Time: 76 ms (45.15%), Space: 38.8MB (69.46%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -10,20 +10,13 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-class Node {
-  constructor(value) {
-    this.value = value,
-    this.left = null,
-    this.right = null
-  }
-}
 const inorderTraversal = (root) => {
   let arr = [];
   const _traverse = (node, arr) => {
-    if (!node || node.value === null) return arr;
-    if (node.left) _traverse(node.left, arr);
-    arr.push(node.val)
-    if (node.right) _traverse(node.right, arr);
+    if (!node) return arr;
+    _traverse(node.left, arr);
+    arr.push(node.val);
+    _traverse(node.right, arr);
   }
   _traverse(root, arr);
   return arr;
