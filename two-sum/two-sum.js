bad5ddf59157b404d73a25fbc1801a86a015bd02
Michael Palmer <33425993+gitInLoserWereGoingShopping@users.noreply.github.com>
2021-09-15 15:28:13 -0500

    Time: 76 ms (86.49%), Space: 40.5 MB (38.90%) - LeetHub
    Optimized speed

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -3,15 +3,31 @@
  * @param {number} target
  * @return {number[]}
  */
+
+//Current solution - Time: 76 ms (86.49%), Space: 40.5 MB (38.90%) - LeetHub
 const twoSum = (nums, target) =>
 {
-  let R = nums.length - 1;
-  for (let L = 0; L < nums.length; L++)
+  let cache = {};
+  cache[nums[0]] = 0;
+  for (let i = 1; i < nums.length; i++)
   {
-    for (let R = L + 1; R < nums.length; R++)
-    {
-      let sum = nums[L] + nums[R];
-      if (sum === target) return [L, R];
-    }
+    let current = nums[i];
+    let neededNum = target - current;
+    if (cache[neededNum] !== undefined) return [i, cache[neededNum]];
+    cache[nums[i]] = i;
   }
 };
+
+//Initial solution - Time: 158 ms (24.04%), Space: 39 MB (96.70%) - LeetHub
+// const twoSum = (nums, target) =>
+// {
+//   let R = nums.length - 1;
+//   for (let L = 0; L < nums.length; L++)
+//   {
+//     for (let R = L + 1; R < nums.length; R++)
+//     {
+//       let sum = nums[L] + nums[R];
+//       if (sum === target) return [L, R];
+//     }
+//   }
+// };
