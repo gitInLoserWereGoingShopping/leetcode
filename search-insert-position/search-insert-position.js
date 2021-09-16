fd0082598feb726b1a007756b0812cab99cebf0a
Michael Palmer <33425993+gitInLoserWereGoingShopping@users.noreply.github.com>
2021-09-16 12:45:36 -0500

    Time: 90 ms (24.97%), Space: 38.6 MB (92.84%) - LeetHub

---
+++ search-insert-position/search-insert-position.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = (nums, target) =>
+{
+  let S = 0;
+  let E = nums.length - 1;
+  let mid = -1;
+  if (nums[E] < target) return nums.length;
+  while (S <= E)
+  {
+    mid = Math.floor((S + E) / 2);
+    let current = nums[mid];
+    if (current === target) return mid;
+    (current < target) ? S++ : E--;
+  }
+  return mid;
+};
