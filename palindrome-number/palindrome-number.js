514cfece314a7aa54836d55c6b9886dc27e2103c
Michael Palmer <33425993+gitInLoserWereGoingShopping@users.noreply.github.com>
2021-10-08 23:09:50 -0500

    Time: 188 ms (67.30%), Space: 47.6 MB (95.08%) - LeetHub

---
+++ palindrome-number/palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = (x) =>
+{
+  let xStr = x.toString();
+  let L = 0;
+  let R = xStr.length - 1;
+  while (L < R)
+  {
+    if (xStr[L] !== xStr[R]) return false;
+    L++;
+    R--;
+  }
+  return true;
+};
