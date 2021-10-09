45e634cd1e02725a7e756c83eea24d4a093d6101
Michael Palmer <33425993+gitInLoserWereGoingShopping@users.noreply.github.com>
2021-10-08 23:26:28 -0500

    Time: 92 ms (62.82%), Space: 46.6 MB (11.93%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = (s) =>
+{
+  let alphaNumStr = ''
+  for (let char of s)
+  {
+    if (char.match(/^[0-9a-zA-Z]+$/)) alphaNumStr += char;
+  }
+  let L = 0;
+  let R = alphaNumStr.length - 1;
+  alphaNumStr = alphaNumStr.toLowerCase();
+  while (L < R)
+  {
+    if (alphaNumStr[L] !== alphaNumStr[R]) return false;
+    L++;
+    R--;
+  }
+  return true;
+};
